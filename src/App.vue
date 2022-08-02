23c8121a977e23e1a230e315e5f6f49463a8f357
leedohyeon0502 <leedohyeon0502@gmail.com>
2022-08-02 17:55:11 +0900

    vue with v-if / v-for

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>hi</h1>
+    <br/>
+
+    <card-ui :CountMenu="3" :CardTitle="'first Card'"/>
+    <card-ui :CountMenu="5"/>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import cardUi from './components/card-ui.vue'
+import CardUi from "./components/card-ui.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { cardUi },
+    CardUi,
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    
 </style>
---
+++ src/components/card-ui.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cont">
+    <h1 v-if="CardTitle">{{CardTitle}}</h1>
+    <h1 v-else>none-title</h1>
+    <r/>
+    <ul>
+        <li v-for="n in CountMenu" :key="n">{{n}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        CountMenu: Number,
+        CardTitle: String,
+    }
+
+
+}
+</script>
+
+<style>
+    .cont{
+        background: blue;
+
+        margin: 100px 0px;
+    }
+    
+</style>
